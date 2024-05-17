b8b3d093878a7b4c076692618e75a8d33e02a032
Chris Tranter <135023953+Chris-Tranter@users.noreply.github.com>
2024-05-17 22:42:02 +0100

    changed char1 and added comments for better understanding

--- script.js
+++ script.js
@@ -1,41 +1,40 @@
 let char1 = {
-    name: "Oliver Twist",
+    name: "Shadow shaman",
     health: 100,
     attacks: [
       {
-        name: "Eye Stab",
-        dialogue: "Here, hold this!",
+        name: "entity sap",
+        dialogue: "The opponents willpower was drained!",
       },
       {
-        name: "Whooping Cough",
-        dialogue: "Catch this!",
+        name: "Shadow shard",
+        dialogue: "The opponents armour was pierced!",
       },
       {
-        name: "Stale Baguette",
-        dialogue: "Please sir, have some more... PAIN.",
+        name: "Ancient gloop",
+        dialogue: "The opponent cannot move!",
       },
     ],
     attack() {
-      let damage = Math.ceil(Math.random() * 10);
+      let damage = Math.ceil(Math.random() * 15);
       let attack = Math.floor(Math.random() * 3);
-      console.log(
-        `"${this.attacks[attack].dialogue}," shouted Oliver, as he used his ${this.attacks[attack].name} attack and dealt ${damage} damage.\n`,
-      );
+      console.log(`${this.name} used ${this.attacks[attack].name}, ${this.attacks[attack].dialogue} which deals ${damage} damage.\n`,);
       return damage;
     },
+    //if char 1 dies to .... then this message is displayed
     die() {
       console.log(
-        `Mighty Hulk picked up Oliver's frail frame and tore the poor little boy in half. "HULK WIIIIINNNNSSSS," he roared.`,
+        `The Shadow shaman falls back, slams onto the floor and vanishes.`,
       );
     },
   };
   
   let char2 = {
-    name: "Hulk Jr",
+    name: "",
     health: 100,
     attacks: [
       {
-        name: "Hulk Smash",
+        name: "",
         dialogue: "HULK SMAAAAAAASH!!!!",
       },
       {
@@ -51,10 +50,11 @@ let char1 = {
       let damage = Math.ceil(Math.random() * 10);
       let attack = Math.floor(Math.random() * 3);
       console.log(
-        `"${this.attacks[attack].dialogue}," shouted Hulk, as he used his ${this.attacks[attack].name} attack and dealt ${damage} damage.\n`,
+        `"${this.attacks[attack].dialogue}," shouted Hulk, as he used his ${this.attacks[attack].name} attack and dealt ${damage} damage.\n`,    // the \n in javascript means new line!
       );
       return damage;
     },
+    //if char 2 dies to .... then this message is displayed
     die() {
       console.log(
         `Oliver took his stale baguette and impaled the mighty figure of Hulk with it. He smiled with delight as the creatures blood soaked the baguette - "yum - that will help with my iron deficiency."`,
@@ -62,18 +62,29 @@ let char1 = {
     },
   };
   
+
+
+// a while loop set to a boolean value
   let gameRunning = true;
   while (gameRunning) {
-    let damageToChar2 = char1.attack();
-    char2.health -= damageToChar2;
-    let damageToChar1 = char2.attack();
-    char1.health -= damageToChar1;
-  
+
+//   first turn char 1 inflicts damage
+      let damageToChar2 = char1.attack();
+// char 2 takes damage
+      char2.health -= damageToChar2;
+//  second turn char 2 inflicts damage
+      let damageToChar1 = char2.attack();
+//  char 1 takes damage    
+      char1.health -= damageToChar1;
+
+//  if char 1 reaches 0 health => game becomes falsy - ends
     if (char1.health <= 0) {
       char1.die();
       gameRunning = false;
+// if char 2 reaches 0 health => game becomes falsy - ends    
     } else if (char2.health <= 0) {
       char2.die();
       gameRunning = false;
     }
+// loop end
   }
